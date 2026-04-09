83ee0bcc08038ef68d998a362177b61e72158822
lbrowni3 <lbrowning853@gmail.com>
2026-04-09 10:29:08 -0500

    Updated greeting message. This message explains what the change is.

--- Logan_Lab1/Logan_Lab1.cpp
+++ Logan_Lab1/Logan_Lab1.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello from Logan! This is my first Git commit.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
